efbbe73e7da47732badeced547f4b0b4ea522f80
MizanRbf <mizanrbf@gmail.com>
2025-11-10 23:32:28 +0600

    problem solved

--- Module-1/dist/test.js
+++ Module-1/dist/test.js
@@ -1,5 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const course = "Next Level web";
-console.log(course);
 //# sourceMappingURL=test.js.map
--- Module-1/src/test.ts
+++
@@ -1,2 +0,0 @@
-const course: string = "Next Level web";
-console.log(course);
